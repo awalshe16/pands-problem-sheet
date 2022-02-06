cde922b3e075112d57302c4cb37cf783c6807fd9
Walshe <adrian.walshe@sap.com>
2022-02-06 19:18:41 +0000

    week 2

---
+++ Week2/testTypes.py
@@ -0,0 +1,13 @@
+#Create a file called testTypes.py, in the file create 5 variables one for each of the following types:
+# int, float, boolean, str, list (look at w3schools.com)#
+i = 5
+fl = 5.5 
+bo = True
+str = "Well lads"
+list = ['cows,dogs, rabbits']
+
+print('variable {} is of type:{} and value:{}'.format(type(i),type(i), i))
+print('variable {} is of type:{} and value:{}'.format(type(fl),type(fl), fl))
+print('variable {} is of type:{} and value:{}'.format(type(bo),type(bo), bo))
+print('variable {} is of type:{} and value:{}'.format(type(str),type(str), str))
+print('variable {} is of type:{} and value:{}'.format(type(list),type(list), list))
